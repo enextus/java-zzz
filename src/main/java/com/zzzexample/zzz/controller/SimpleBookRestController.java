582a36904695636b65f1707f6b6f6793cedd5bd8
enextus <enextus@gmail.com>
2022-10-26 23:03:45 +0200

    pom

--- src/main/java/com/zzzexample/zzz/controller/SimpleBookRestController.java
+++ src/main/java/com/zzzexample/zzz/controller/SimpleBookRestController.java
@@ -1,26 +1,29 @@
 package com.zzzexample.zzz.controller;
 
 import com.zzzexample.zzz.entyties.Book;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value ="books-rest")
+@RequestMapping(value = "rest")
 public class SimpleBookRestController {
 
-    @GetMapping(value = "/{id}", produces = "application/json")
-    public Book getBook(@PathVariable int id) {
-        return findBookById(id);
-    }
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public Book getBook(@PathVariable int id) {
+		System.out.println("\ngetBook " + id + ";\n");
+		return findBookById(id);
+	}
 
-    private Book findBookById() {
-        return findBookById(0);
-    }
+	private Book findBookById() {
+		return findBookById(0);
+	}
 
-    private Book findBookById(int id) {
-        return new Book();
-    }
+	private Book findBookById(int id) {
+		System.out.println("\nfindBookById " + id + ";\n");
+		return new Book(id);
+	}
 
 }
--- src/main/java/com/zzzexample/zzz/entyties/Book.java
+++ src/main/java/com/zzzexample/zzz/entyties/Book.java
@@ -1,15 +1,30 @@
 package com.zzzexample.zzz.entyties;
 
 public class Book {
-    final String fiStr = "Fuck you, bitch!";
+	final String fiStr = "Fuck you, bitch!";
+	int idd = 1;
 
-    public String getFiStr() {
-        return fiStr;
-    }
+	{
+		System.out.println("Блок в классе Book.");
+	}
 
-    private Book findBookById(int id) {
-        id = 0;
-        return new Book();
-    }
+	public Book(int idd) {
+		System.out.println("IDD " + idd);
 
+		this.idd = idd;
+		System.out.println("this.idd " + this.idd);
+	}
+
+
+	public String getFiStr() {
+		return fiStr;
+	}
+
+	private Book findBookById(int id) {
+		return new Book(id + idd);
+	}
+
+	{
+		System.out.println("2. блок " + idd);
+	}
 }
