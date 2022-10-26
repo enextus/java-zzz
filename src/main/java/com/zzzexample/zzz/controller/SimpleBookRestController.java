4849279bf17e6790017754fbeec8d9f594207e35
enextus <enextus@gmail.com>
2022-10-26 22:18:23 +0200

    main

---
+++ src/main/java/com/zzzexample/zzz/controller/SimpleBookRestController.java
@@ -0,0 +1,26 @@
+package com.zzzexample.zzz.controller;
+
+import com.zzzexample.zzz.entyties.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="books-rest")
+public class SimpleBookRestController {
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public Book getBook(@PathVariable int id) {
+        return findBookById(id);
+    }
+
+    private Book findBookById() {
+        return findBookById(0);
+    }
+
+    private Book findBookById(int id) {
+        return new Book();
+    }
+
+}
---
+++ src/main/java/com/zzzexample/zzz/entyties/Book.java
@@ -0,0 +1,15 @@
+package com.zzzexample.zzz.entyties;
+
+public class Book {
+    final String fiStr = "Fuck you, bitch!";
+
+    public String getFiStr() {
+        return fiStr;
+    }
+
+    private Book findBookById(int id) {
+        id = 0;
+        return new Book();
+    }
+
+}
